eff35c67114cca5493bf67fffc1591e6aa87baf4
João Amadeu <jmamadeu2000@gmail.com>
2022-02-15 04:52:04 +0100

    chore: prettify code

--- src/app/features/tv-shows/index.ts
+++ src/app/features/tv-shows/index.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
 import { TvShowAPIResponseType, TvShowType } from "./types";
 import { DEFAULT_TV_SHOW, parseTVShow } from "./utils";
 
--- src/app/features/tv-shows/utils.ts
+++ src/app/features/tv-shows/utils.ts
@@ -1,9 +1,9 @@
-import { TVShowAPIResponseType, TVShowType } from "./types";
+import { TvShowAPIResponseType, TvShowType } from "./types";
 
 export const DEFAULT_TV_SHOW = "Powerpuff Girls";
 
-export const parseTVShow = (tvShow: TVShowAPIResponseType): TVShowType => {
-  const tvShowParsed: TVShowType = {
+export const parseTVShow = (tvShow: TvShowAPIResponseType): TvShowType => {
+  const tvShowParsed: TvShowType = {
     id: tvShow?.id,
     coverImageURL: tvShow?.image?.medium,
     description: tvShow?.summary,
